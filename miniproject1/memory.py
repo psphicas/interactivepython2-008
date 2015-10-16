f6d9829e39a06823cf4373f4baf4d6c67c0b6805
Phil G. Sphicas <pgs2111@columbia.edu>
2015-10-15 22:15:54 -0700

    draw the cards

--- miniproject1/memory.py
+++ miniproject1/memory.py
@@ -5,19 +5,37 @@ import random
 
 # helper function to initialize globals
 def new_game():
-    pass  
+    global deck, exposed
+    
+    # create the deck of 16 numbers
+    deck = range(0,8) + range(0,8)
+    
+    # shuffle the deck
+    random.shuffle(deck)
+    
+    # all the cards start face-down
+    exposed = [False] * 16
 
      
 # define event handlers
 def mouseclick(pos):
-    # add game state logic here
-    pass
-    
-                        
+    global deck
+    card = pos[0] // 50
+    exposed[card] = not exposed[card]
+
+
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
-    pass
-
+    global deck
+    for i, card in enumerate(deck):
+        if exposed[i]:
+            canvas.draw_text(str(card), (50*i + 25,50), 24, "Red")
+        else:
+            canvas.draw_polygon([(i*50,0),
+                              (i*50,100),
+                              (i*50+50,100),
+                              (i*50+50,0)],
+                             1, "Black", "Green")
 
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 800, 100)
@@ -32,5 +50,6 @@ frame.set_draw_handler(draw)
 new_game()
 frame.start()
 
-
 # Always remember to review the grading rubric
+
+# http://www.codeskulptor.org/#user40_sNVLIdVnZC_0.py
